5eb58196a7575b4d8739f9584744d842e71fdd33
nackie97 <98790770+nackie97@users.noreply.github.com>
2022-12-29 17:41:36 +0700

    Add files via upload

---
+++ Chatbot/README.md
@@ -0,0 +1,16 @@
+How to run
+
+--Run Chatbot
+1.Install requirements.txt
+2.Run in terminal using command -> python chat.py
+3.Type 'quite' for exit program
+
+--Train modal
+1.Use chatbot.ipynb
+
+Develope by Group 1 
+
+นายธีรคม ถวิลวิสาร รหัส 6409035059
+นางสาวนภัสวรรณ อักษรเดช รหัส 6409035133
+นายนรุตม์ชัย เรืองญาณ รหัส 6409035216
+
---
+++ Chatbot/chat.py
@@ -0,0 +1,89 @@
+import json 
+import numpy as np
+from tensorflow import keras
+from sklearn.preprocessing import LabelEncoder
+
+import colorama 
+colorama.init()
+from colorama import Fore, Style, Back
+
+import random
+import pickle
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+stemmer = LancasterStemmer()
+
+# getting informations from intents.json--
+words = []
+labels = []
+x_docs = []
+y_docs = []
+
+for intent in data['intents']:
+    for pattern in intent['patterns']:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        x_docs.append(wrds)
+        y_docs.append(intent['tag'])
+
+        if intent['tag'] not in labels:
+            labels.append(intent['tag'])
+
+# Stemming the words and removing duplicate elements.
+words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
+words = sorted(list(set(words)))
+labels = sorted(labels)
+
+
+
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for s_word in s_words:
+        for i, w in enumerate(words):
+            if w == s_word:
+                bag[i] = 1
+
+    return np.array(bag)
+
+
+def chat():
+    # load trained model
+    model = keras.models.load_model('chat_model')
+
+    # load tokenizer object
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+    # load label encoder object
+    with open('label_encoder.pickle', 'rb') as enc:
+        lbl_encoder = pickle.load(enc)
+
+    # parameters
+    max_len = 20
+    
+    while True:
+        print(Fore.LIGHTBLUE_EX + "User: " + Style.RESET_ALL, end="")
+        inp = input()
+        if inp.lower() == "quit":
+            break
+
+        result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([inp]),
+                                             truncating='post', maxlen=max_len))
+        tag = lbl_encoder.inverse_transform([np.argmax(result)])
+
+        for i in data['intents']:
+            if i['tag'] == tag:
+                print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL , np.random.choice(i['responses']))
+
+        # print(Fore.GREEN + "ChatBot:" + Style.RESET_ALL,random.choice(responses))
+
+print(Fore.YELLOW + "Start messaging with the bot (type quit to stop)!" + Style.RESET_ALL)
+chat()
